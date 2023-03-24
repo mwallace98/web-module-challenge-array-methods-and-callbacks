9c50734fe658b5b901926a9418fb5849084d32dc
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-03-23 21:25:40 -0400

    tasks 1-3 complete

--- index.js
+++ index.js
@@ -7,16 +7,32 @@ Practice accessing data by console.log-ing the following pieces of data note.
 
 💡 HINT: You may want to filter the data first 😉*/
 
+
+const finals2014 = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
+    });
+    
+    console.log(finals2014);
 //(a) Home Team name for 2014 world cup final
+
+console.log(finals2014[0]['Home Team Name']);
+
 
 //(b) Away Team name for 2014 world cup final
 
+console.log(finals2014[0]['Away Team Name']);
+
 //(c) Home Team goals for 2014 world cup final
+
+console.log(finals2014[0]['Home Team Goals']);
 
 //(d) Away Team goals for 2014 world cup final
 
+console.log(finals2014[0]['Away Team Goals']);
+
 //(e) Winner of 2014 world cup final */
 
+console.log(finals2014[0]['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -26,10 +42,15 @@ Use getFinals to do the following:
 💡 HINT - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-    /* code here */
- }
 
+function getFinals(array) {
+    const finalsTeams = array.filter(function(obj1) {
+        return obj1.Stage == 'Final'
+    });
+    return finalsTeams;
+    }
+ 
+console.log('test', getFinals(fifaData));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -38,11 +59,16 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function as the second parameter that will take getFinals from task 2 as an argument
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array,callBack) {
+    const getfinalsdataset = callBack(array); 
+    const years = getfinalsdataset.map(newfunction);
+   return years
 }
-
-
+   function newfunction(obj2){
+    return obj2.Year
+   }
+    
+console.log('task 3', getYears(fifaData, getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
