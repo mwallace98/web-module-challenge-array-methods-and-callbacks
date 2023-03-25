60afbfa60494d8ab29ccd208eaa810734292d50f
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-03-25 16:41:13 -0400

    All Taks Complete

--- index.js
+++ index.js
@@ -122,11 +122,14 @@ Use the higher order function `getAverageGoals` to do the following:
  
 */
 
-function getAverageGoals(/* code here */) {
-    /* code here */
+function getAverageGoals(data) {
+    const averageHomeGoals = data.reduce(function(acc,item){
+        return acc + item['Home Team Goals'] + item['Away Team Goals'];
+    },0)
+    return (averageHomeGoals / data.length).toFixed(2);
  }
 
-
+console.log(getAverageGoals(fifaData));
 
 
 /// 🥅 STRETCH 🥅 ///
